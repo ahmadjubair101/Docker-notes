232bc4f98ea1b671f94fe41feaca50bdeded69cb
codercogit <ahmadjubair101@gmail.com>
2026-07-03 17:57:41 +0100

    Add Flask UI, templates, nginx setup, and Redis counter

--- coderco-challenge/count.py
+++ coderco-challenge/count.py
@@ -1,22 +1,21 @@
-from flask import Flask  # type: ignore[import]
-import redis  # type: ignore[import]
+from flask import Flask, render_template
+import redis
 import os
 
 app = Flask(__name__)
-redis_host = os.getenv('REDIS_HOST', 'redis')
-redis_port = int(os.getenv('REDIS_PORT', 6379))
+
+redis_host = os.getenv("REDIS_HOST", "redis")
+redis_port = int(os.getenv("REDIS_PORT", 6379))
 redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
 
-@app.route('/')
+@app.route("/")
 def welcome():
-    return "Welcome to the CoderCo Container Challenge!"
+    return render_template("index.html")
 
-@app.route('/count')
+@app.route("/count")
 def count():
-    count = redis_client.incr('visits')
-    return f'This page has been visited {count} times.'
+    visits = redis_client.incr("visits")
+    return render_template("count.html", visits=visits)
 
-
-
-if __name__ == '__main__': 
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
